5f35cd6cb099196663166b3a71001a7236e3fb3d
partydragen <admin@partydragen.com>
2017-12-30 18:10:58 +0100

    Add files via upload

---
+++ Website/PlayerServers/EnglishUK/psmanager.php
@@ -0,0 +1,31 @@
+<?php 
+$language = array( 
+	'filemanager' => 'File Manager',
+	'dashboard' => 'Dashboard',
+	'welcome' => 'Welcome',
+	'server_name' => '<b>Server Name:</b>',
+	'last_online' => '<b>Last Online:</b>',
+	'plan' => '<b>Plan:</b>',
+	'plans' => '<b>Plans:</b>',
+	'status' => '<b>Status</b>',
+	'players' => '<b>Players</b>',
+	'none_files_or_folders' => 'No files or folders could be found in this directory',
+	'edit' => '<i class="fa fa-pencil" aria-hidden="true"></i> Edit',
+	'successfully_updated' => 'Successfully Updated',
+	
+	'file_name' => 'File Name',
+	'content' => 'Content',
+	'save' => 'Save',
+	'cancel' => 'Cancel',
+	'delete' => 'Delete',
+	
+	'confirm_deletion' => 'Confirm deletion',
+	'delete_message' => 'Are you sure you want to delete',
+	'successfully_updated' => 'Successfully Updated',
+
+	// errors
+	'successfully_deleted' => 'File/Folder successfully deleted',
+	'only_mvpplus_function' => 'Sorry, You need to be a MVP+ donator to use download function',
+	'cant_delete' => 'Sorry, You can\'t delete plugins folder',
+	'cant_rename_this' => 'Sorry, You can\'t rename plugin folder',
+);
