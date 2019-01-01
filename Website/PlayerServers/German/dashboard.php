dbe1688eecb963c9477376c7253f39cab3351d25
Tobi406 <mayertobias06@gmail.com>
2019-01-01 13:21:45 +0100

    Added German language. With errors contact me or another german speaker (on CubedCraft), my discord is Tobi#8249

---
+++ Website/PlayerServers/German/dashboard.php
@@ -0,0 +1,114 @@
+<?php 
+$language = array(
+	// Generel
+	'dashboard' => 'Dashboard',
+	'plans' => 'Plans',
+	'enter' => 'Enter',
+	'actions' => 'Actions',
+	'console' => 'Console',
+	'success' => 'Success',
+	'error' => 'Error',
+	'save' => 'Save',
+	'cancel' => 'Cancel',
+	'delete' => 'Delete',
+	'successfully_updated' => 'Successfully Updated',
+	'yes' => 'Yes',
+	'no' => 'No',
+
+	// dashboard
+	'server_details' => 'Server Details',
+	'server_status' => 'Server Status',
+	'connected_players' => 'Connected Players',
+	'server_name_x' => '<strong>Server Name:</strong> {x}', // Don't replace {x}
+	'plan_x' => '<strong>Server Plan:</strong> {x}', // Don't replace {x}
+	'version_x' => '<strong>Server Version:</strong> {x}', // Don't replace {x}
+	'motd_x' => '<strong>Motd:</strong> {x}', // Don't replace {x}
+	'created_x' => '<strong>Created:</strong> {x}', // Don't replace {x}
+	'last_online_x' => '<strong>Last online:</strong> {x}', // Don't replace {x}
+	'votes_x' => '<strong>Votes:</strong> {x}', // Don't replace {x}
+	'status_x' => '<strong>Status:</strong> {x}', // Don't replace {x}
+	'players_x' => '<strong>Players:</strong> {x}', // Don't replace {x}
+	'connect_with_x' => '<strong>Connect with:</strong> {x}', // Don't replace {x}
+	'change' => '(Change)',
+	
+	// User management
+	'server_managers' => 'Server Managers',
+	'server_managers_info' => 'The following users have full access to manage your server. As you are on the <strong>{x} Plan</strong>, you can add up to <strong>{y}</strong> users.',
+	'user' => 'User',
+	'add_player' => 'Add Player',
+	'remove_player' => 'Remove',
+	'confirm_remove_player' => 'Are you sure you want to remove this player from your server\'s management?',
+	'confirm_add_player' => 'Are you sure you want to add this player to your server\'s management? Any changes they make cannot be undone!',
+	'no_users_added' => 'No users have been added to your server yet.',
+	'enter_username' => 'Please enter a valid username.',
+	'cant_add_self' => 'You can\'t add yourself to your own server!',
+	'already_added_user' => 'You have already added that user to your server.',
+	'other_user_hasnt_registered' => 'That user hasn\'t registered on our website yet, or their website username is different to the one you have entered.',
+	'user_limit_{x}' => 'Unable to add another user, you have reached the limit of {x} for your plan.',
+	'owner_only_add_managers' => 'Only the server owner can manage this server\'s users!',
+	
+	// Filemanager
+	'filemanager' => 'File Manager',
+	'new_file' => 'New File',
+	'edit' => 'Edit',
+	'upload_file' => 'Upload File',
+	'file_name' => 'File Name',
+	'content' => 'Content',
+	'fileorfolder' => 'File/Folder',
+	'successfully_updated' => 'File successfully updated',
+	'successfully_created' => 'File successfully created',
+	'successfully_deleted' => 'File/Folder successfully deleted',
+	'confirm_deletion' => 'Confirm deletion',
+	'delete_message' => 'Are you sure you want to delete',
+	'confirm_cancel' => 'Are you sure you want to cancel?',
+	'cant_delete' => 'Sorry, you can\'t delete the plugins folder',
+	'cant_rename_this' => 'Sorry, you can\'t rename the plugins folder',
+	'cannot_upload_to_plugins_folder' => 'Sorry, You can\'t upload files to plugins folder!',
+	'folder_not_found' => 'Could not find the folder!, Make sure it exist',
+	'cant_download_plugins_folder' => 'You can\'t download the plugins folder for security reasons',
+	'none_files_or_folders' => 'No files or folders could be found in this directory',
+	'invalid_extension' => 'The file extension is invalid. Allowed extensions: {x}', // Don't replace {x}
+	
+	// Properties
+	'properties' => 'Properties',
+	'properties_updated_successfully' => 'Properties updated successfully',
+	'enable_command_blocks' => 'Enable command blocks?',
+	'enable_pvp' => 'Enable PvP?',
+	'level_type' => 'Level Type',
+	'default' => 'Default',
+	'flat' => 'Flat',
+	'large_biomes' => 'Large Biomes',
+	'amplified' => 'Amplified',
+	'gamemode' => 'Gamemode',
+	'survival' => 'Survival',
+	'creative' => 'Creative',
+	'adventure' => 'Adventure',
+	'spectator' => 'Spectator',
+	'force_gamemode' => 'Force Gamemode?',
+	'enable_monster_spawning' => 'Enable monster spawning?',
+	'enable_animal_spawning' => 'Enable animal spawning?',
+	'enable_flight' => 'Allow flight?',
+	'difficulty' => 'Difficulty',
+	'peaceful' => 'Peaceful',
+	'easy' => 'Easy',
+	'normal' => 'Normal',
+	'hard' => 'Hard',
+	'announce_player_achievements' => 'Announce player achievements?',
+	'allow_nether' => 'Enable Nether?',
+	'generate_structures' => 'Generate structures?',
+	'resource_pack' => 'Resource pack url',
+	'generator_settings' => 'Generator settings',
+	'spawn_protection' => 'Spawn Protection',
+	
+	// Plugins
+	'plugins' => 'Plugins',
+	'name' => 'Name',
+	'description' => 'Description',
+	'install' => 'Install',
+	'remove' => 'Remove',
+	
+	// Server settings
+	'settings' => 'Settings',
+	'server_motd_required' => 'A motd is required.',
+	'server_motd_max' => 'Motd must be a maximum of 32 characters.',
+);
