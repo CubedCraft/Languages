3f109b789a7d86b3293a86bb01ed79caa2741d57
Tobi406 <mayertobias06@gmail.com>
2019-01-01 13:25:06 +0100

    Added the dashbord string.

--- Website/PlayerServers/German/dashboard.php
+++ Website/PlayerServers/German/dashboard.php
@@ -1,114 +1,114 @@
-<?php 
+<?php
 $language = array(
 	// Generel
-	'dashboard' => 'Dashboard',
-	'plans' => 'Plans',
+	'dashboard' => 'Kontrollbrett',
+	'plans' => 'Pläne',
 	'enter' => 'Enter',
-	'actions' => 'Actions',
-	'console' => 'Console',
-	'success' => 'Success',
-	'error' => 'Error',
-	'save' => 'Save',
-	'cancel' => 'Cancel',
-	'delete' => 'Delete',
-	'successfully_updated' => 'Successfully Updated',
-	'yes' => 'Yes',
-	'no' => 'No',
+	'actions' => 'Aktionen',
+	'console' => 'Konsole',
+	'success' => 'Erfolg',
+	'error' => 'Fehler',
+	'save' => 'Speichern',
+	'cancel' => 'Abbrechen',
+	'delete' => 'Löschen',
+	'successfully_updated' => 'Erfolgreich aktualisiert',
+	'yes' => 'Ja',
+	'no' => 'Nein',
 
 	// dashboard
-	'server_details' => 'Server Details',
-	'server_status' => 'Server Status',
-	'connected_players' => 'Connected Players',
-	'server_name_x' => '<strong>Server Name:</strong> {x}', // Don't replace {x}
-	'plan_x' => '<strong>Server Plan:</strong> {x}', // Don't replace {x}
-	'version_x' => '<strong>Server Version:</strong> {x}', // Don't replace {x}
+	'server_details' => 'Serverdetails',
+	'server_status' => 'Serverstatus',
+	'connected_players' => 'Verbunde Spieler',
+	'server_name_x' => '<strong>Servername:</strong> {x}', // Don't replace {x}
+	'plan_x' => '<strong>Serverplan:</strong> {x}', // Don't replace {x}
+	'version_x' => '<strong>Serverversion:</strong> {x}', // Don't replace {x}
 	'motd_x' => '<strong>Motd:</strong> {x}', // Don't replace {x}
-	'created_x' => '<strong>Created:</strong> {x}', // Don't replace {x}
-	'last_online_x' => '<strong>Last online:</strong> {x}', // Don't replace {x}
+	'created_x' => '<strong>Erstellt:</strong> {x}', // Don't replace {x}
+	'last_online_x' => '<strong>Zuletzt online:</strong> {x}', // Don't replace {x}
 	'votes_x' => '<strong>Votes:</strong> {x}', // Don't replace {x}
 	'status_x' => '<strong>Status:</strong> {x}', // Don't replace {x}
-	'players_x' => '<strong>Players:</strong> {x}', // Don't replace {x}
-	'connect_with_x' => '<strong>Connect with:</strong> {x}', // Don't replace {x}
-	'change' => '(Change)',
-	
+	'players_x' => '<strong>Spieler:</strong> {x}', // Don't replace {x}
+	'connect_with_x' => '<strong>Verbinde mit:</strong> {x}', // Don't replace {x}
+	'change' => '(Wechseln)',
+
 	// User management
-	'server_managers' => 'Server Managers',
-	'server_managers_info' => 'The following users have full access to manage your server. As you are on the <strong>{x} Plan</strong>, you can add up to <strong>{y}</strong> users.',
-	'user' => 'User',
-	'add_player' => 'Add Player',
-	'remove_player' => 'Remove',
-	'confirm_remove_player' => 'Are you sure you want to remove this player from your server\'s management?',
-	'confirm_add_player' => 'Are you sure you want to add this player to your server\'s management? Any changes they make cannot be undone!',
-	'no_users_added' => 'No users have been added to your server yet.',
-	'enter_username' => 'Please enter a valid username.',
-	'cant_add_self' => 'You can\'t add yourself to your own server!',
-	'already_added_user' => 'You have already added that user to your server.',
-	'other_user_hasnt_registered' => 'That user hasn\'t registered on our website yet, or their website username is different to the one you have entered.',
-	'user_limit_{x}' => 'Unable to add another user, you have reached the limit of {x} for your plan.',
-	'owner_only_add_managers' => 'Only the server owner can manage this server\'s users!',
-	
+	'server_managers' => 'Serververwalter',
+	'server_managers_info' => 'Die folgenden Benutzer haben volle Rechte deinen Server zu verwalten. Da du den <strong>{x} Plan</strong> hast, kannst du bis zu <strong>{y}</strong> Benutzer hinzufügen.',
+	'user' => 'Benutzer',
+	'add_player' => 'Füge Spieler hinzu',
+	'remove_player' => 'Entfernen',
+	'confirm_remove_player' => 'Bist du sicher, das du diesen Spieler von deiner Serververwaltung entfernen möchtest?',
+	'confirm_add_player' => 'Bist du sicher, das du diesen Spieler zu deiner Serververwaltung hinzufügen möchtest? Veränderungen, die sie machen, können nicht rückgängig gemacht werden!',
+	'no_users_added' => 'Keine Benutzer wurden zu deinem Server hinzugefügt.',
+	'enter_username' => 'Bitte gebe einen existierenden Benutzernamen an.',
+	'cant_add_self' => 'Du kannst dich selber nicht zu deinem eigenen Server hinzufügen!',
+	'already_added_user' => 'Du hast diesen Spieler schon zu deinem Server hinzugefügt.',
+	'other_user_hasnt_registered' => 'Dieser Benztzer hat sich nicht auf unserer Webseite registriert oder sein Webseitenbenutzername ist anders, als denn, den du eingegeben hast.',
+	'user_limit_{x}' => 'Kann keinen weiteren Benutzer hinzufügen, du hast das Limit von {x} für deinen Plan erreicht.',
+	'owner_only_add_managers' => 'Nur der Serverbesitzer kann die Serververwalter verwalten!',
+
 	// Filemanager
-	'filemanager' => 'File Manager',
-	'new_file' => 'New File',
-	'edit' => 'Edit',
-	'upload_file' => 'Upload File',
-	'file_name' => 'File Name',
-	'content' => 'Content',
-	'fileorfolder' => 'File/Folder',
-	'successfully_updated' => 'File successfully updated',
-	'successfully_created' => 'File successfully created',
-	'successfully_deleted' => 'File/Folder successfully deleted',
-	'confirm_deletion' => 'Confirm deletion',
-	'delete_message' => 'Are you sure you want to delete',
-	'confirm_cancel' => 'Are you sure you want to cancel?',
-	'cant_delete' => 'Sorry, you can\'t delete the plugins folder',
-	'cant_rename_this' => 'Sorry, you can\'t rename the plugins folder',
-	'cannot_upload_to_plugins_folder' => 'Sorry, You can\'t upload files to plugins folder!',
-	'folder_not_found' => 'Could not find the folder!, Make sure it exist',
-	'cant_download_plugins_folder' => 'You can\'t download the plugins folder for security reasons',
-	'none_files_or_folders' => 'No files or folders could be found in this directory',
-	'invalid_extension' => 'The file extension is invalid. Allowed extensions: {x}', // Don't replace {x}
-	
+	'filemanager' => 'Dateiverwalter',
+	'new_file' => 'Neue Datei',
+	'edit' => 'Editieren',
+	'upload_file' => 'Datei hochladen',
+	'file_name' => 'Dateiname',
+	'content' => 'Inhalt',
+	'fileorfolder' => 'Datei/Ordner',
+	'successfully_updated' => 'Datei erfolgreich aktualisiert',
+	'successfully_created' => 'Datei erfolgreich erstellt',
+	'successfully_deleted' => 'Datei/Ordner erfolgreich gelöscht',
+	'confirm_deletion' => 'Löschen bestätigen',
+	'delete_message' => 'Bist du dir sicher, das zu löschen',
+	'confirm_cancel' => 'Bist du sicher, das du abbrechen möchtestl?',
+	'cant_delete' => 'Entschuldigung, du kannst den Pluginorder nicht löschen' ,
+	'cant_rename_this' => 'Entschuldigung, du kannst den Pluginorder nicht umbenennen',
+	'cannot_upload_to_plugins_folder' => 'Entschuldigung, du kannst keine Dateien zum Pluginordner hochladen!',
+	'folder_not_found' => 'Dieser Ordner konnte nicht gefunden werden!, Mache sicher, das er existiert',
+	'cant_download_plugins_folder' => 'Du kannst den Pluginorder aufgrund von Sicherheitsgründen nicht herunterladen',
+	'none_files_or_folders' => 'Es konnten keine Dateien oder Ordner in diesem Verzeichnis gefunden werden',
+	'invalid_extension' => 'Diese Dateiendung ist ungültig. Gültige Endungen sind: {x}', // Don't replace {x}
+
 	// Properties
-	'properties' => 'Properties',
-	'properties_updated_successfully' => 'Properties updated successfully',
-	'enable_command_blocks' => 'Enable command blocks?',
-	'enable_pvp' => 'Enable PvP?',
-	'level_type' => 'Level Type',
-	'default' => 'Default',
-	'flat' => 'Flat',
-	'large_biomes' => 'Large Biomes',
-	'amplified' => 'Amplified',
-	'gamemode' => 'Gamemode',
-	'survival' => 'Survival',
-	'creative' => 'Creative',
+	'properties' => 'Eigenschaften',
+	'properties_updated_successfully' => 'Eigenschaften erfolgreich aktualisiert',
+	'enable_command_blocks' => 'Befehleblöscke aktivieren?',
+	'enable_pvp' => 'PvP aktivieren?',
+	'level_type' => 'Leveltyp',
+	'default' => 'Standard',
+	'flat' => 'Flach',
+	'large_biomes' => 'Große Biome',
+	'amplified' => 'Zerklüftet',
+	'gamemode' => 'Spielmodus',
+	'survival' => 'Überlebensmodus',
+	'creative' => 'Kreativmodus',
 	'adventure' => 'Adventure',
-	'spectator' => 'Spectator',
+	'spectator' => 'Zuschauermodus',
 	'force_gamemode' => 'Force Gamemode?',
-	'enable_monster_spawning' => 'Enable monster spawning?',
-	'enable_animal_spawning' => 'Enable animal spawning?',
-	'enable_flight' => 'Allow flight?',
-	'difficulty' => 'Difficulty',
-	'peaceful' => 'Peaceful',
-	'easy' => 'Easy',
+	'enable_monster_spawning' => 'Monsterspawnen erlauben?',
+	'enable_animal_spawning' => 'Tierspawnen erlauben?',
+	'enable_flight' => 'Fliegen erlauben?',
+	'difficulty' => 'Schwierigkeitsgrad',
+	'peaceful' => 'Friedlich',
+	'easy' => 'Einfach',
 	'normal' => 'Normal',
-	'hard' => 'Hard',
-	'announce_player_achievements' => 'Announce player achievements?',
-	'allow_nether' => 'Enable Nether?',
-	'generate_structures' => 'Generate structures?',
-	'resource_pack' => 'Resource pack url',
-	'generator_settings' => 'Generator settings',
-	'spawn_protection' => 'Spawn Protection',
-	
+	'hard' => 'Hart',
+	'announce_player_achievements' => 'Spielererfolge bekannt geben?',
+	'allow_nether' => 'Nether aktivieren?',
+	'generate_structures' => 'Strukturen generieren?',
+	'resource_pack' => 'Ressourcepaket url',
+	'generator_settings' => 'Generatoreinstellungen',
+	'spawn_protection' => 'Spawnschutz',
+
 	// Plugins
 	'plugins' => 'Plugins',
 	'name' => 'Name',
-	'description' => 'Description',
-	'install' => 'Install',
-	'remove' => 'Remove',
-	
+	'description' => 'Beschreibung',
+	'install' => 'Installieren',
+	'remove' => 'Entfernen',
+
 	// Server settings
-	'settings' => 'Settings',
-	'server_motd_required' => 'A motd is required.',
-	'server_motd_max' => 'Motd must be a maximum of 32 characters.',
+	'settings' => 'Einstellungen',
+	'server_motd_required' => 'Eine Motd ist benötigt.',
+	'server_motd_max' => 'Die Motd darf aus maximal 32 Zeichen bestehen.',
 );
